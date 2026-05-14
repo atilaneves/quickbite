5933c46ca891250cbfab2d44b8e3c1f012974448
Atila Neves <atila.neves@gmail.com>
2026-05-15 00:59:45 +0200

    Run IR cerealed enums test

--- tests/ut/cerealed.d
+++ tests/ut/cerealed.d
@@ -68,6 +68,7 @@ private enum bool shouldFailCerealedTest(
             fileName != "cerealiser_impl.d" &&
             fileName != "bugs.d" &&
             fileName != "encode_decode.d" &&
+            fileName != "enums.d" &&
             fileName != "utils.d"
         )
     );
