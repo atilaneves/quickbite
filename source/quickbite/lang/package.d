2334649a349f99634b4bbcd04bafe9a1e5a60205
Atila Neves <atila.neves@gmail.com>
2026-05-26 19:08:41 +0200

    Update Value in lang

--- source/quickbite/lang/package.d
+++ source/quickbite/lang/package.d
@@ -3,30 +3,66 @@ module quickbite.lang;
 private:
 
 
-public alias Value = imported!"std.sumtype".SumType!(
+public struct Value {
+    private alias Data = imported!"std.sumtype".SumType!(
 
-    Void,
+        Void,
 
-    bool,
+        bool,
 
-    ubyte,
-    byte,
-    short,
-    ushort,
-    int,
-    uint,
-    long,
-    ulong,
+        ubyte,
+        byte,
+        short,
+        ushort,
+        int,
+        uint,
+        long,
+        ulong,
 
-    char,
-    wchar,
-    dchar,
+        char,
+        wchar,
+        dchar,
 
-    float,
-    double,
-    real,
+        float,
+        double,
+        real,
 
-);
+        Array,
+    );
+
+    private Data data;
+
+    public this(T)(in T value) @safe pure
+    if (!is(T == E[], E))
+    {
+        data = Data(value);
+    }
+
+    public this(T)(in T[] values) @safe pure {
+        Value[] elements;
+        foreach (value; values)
+            elements ~= Value(value);
+
+        data = Data(Array(elements));
+    }
+
+    public bool opEquals(in Value other) const @safe pure {
+        return data == other.data;
+    }
+
+    public string toString() const @safe pure {
+        return data.toString;
+    }
+}
+
+
+private struct Array {
+    public Value[] elements;
+
+    public this(in Value[] elements) @safe pure {
+        this.elements = elements.dup;
+    }
+}
 
 
 public struct Void {}
--- tests/ut/lang.d
+++ tests/ut/lang.d
@@ -6,14 +6,14 @@ import quickbite.lang;
 
 
 @("value.void")
-unittest {
+@safe pure unittest {
     Value(Void.init).should == Value(Void.init);
     Value(Void.init).should.not == Value(false);
     Value(Void.init).should.not == Value(0);
 }
 
 @("value.bool")
-unittest {
+@safe pure unittest {
     Value(false).should == Value(false);
     Value(true).should.not == Value(false);
 }
@@ -26,7 +26,7 @@ static foreach(
 )
 {
     @("value.integer." ~ T.stringof)
-    unittest {
+    @safe pure unittest {
         const T val = 42;
         Value(val).should == Value(val);
         Value(cast(T) 7).should.not == Value(val);
@@ -35,7 +35,7 @@ static foreach(
 
 static foreach(T; imported!"std.meta".AliasSeq!(char, wchar, dchar)) {
     @("value.char." ~ T.stringof)
-    unittest {
+    @safe pure unittest {
         const val = cast(T) 42;
         Value(val).should == Value(val);
         Value(cast(T) 7).should.not == Value(val);
@@ -45,9 +45,27 @@ static foreach(T; imported!"std.meta".AliasSeq!(char, wchar, dchar)) {
 
 static foreach(T; imported!"std.meta".AliasSeq!(float, double, real)) {
     @("value.float." ~ T.stringof)
-    unittest {
+    @safe pure unittest {
         const val = cast(T) 42;
         Value(val).should == Value(val);
         Value(cast(T) 7).should.not == Value(val);
     }
+}
+
+@("value.array.d.bool")
+@safe pure unittest {
+    Value([false, true]).should == Value([false, true]);
+    Value([true, false]).should.not == Value([false, true]);
+}
+
+@("value.array.d.int")
+@safe pure unittest {
+    Value([1, 2]).should == Value([1, 2]);
+    Value([2, 3]).should.not == Value([1, 2]);
+}
+
+@("value.array.d.double")
+@safe pure unittest {
+    Value([1.1, 2.2]).should == Value([1.1, 2.2]);
+    Value([2.2, 3.3]).should.not == Value([1.1, 2.2]);
 }
