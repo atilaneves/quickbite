f95b3e2db1453a4cbc070fc940ca9f7b3586c774
Atila Neves <atila.neves@gmail.com>
2026-05-28 19:11:26 +0200

    Migrate initial cerealed CTFE backend files

--- tests/ut/backends/deps/cerealed.d
+++ tests/ut/backends/deps/cerealed.d
@@ -9,6 +9,22 @@ import ut.dub_paths: cerealTestsDir, dubImportPaths;
 private:
 
 static foreach (backend; backends) {
+    @("cerealed.bugs.d." ~ backend.stringof)
+    unittest {
+        runBackendFileFixtureTests!backend(
+            buildPath(cerealTestsDir, "bugs.d"),
+            dubImportPaths,
+        );
+    }
+
+    @("cerealed.cerealiser_impl.d." ~ backend.stringof)
+    unittest {
+        runBackendFileFixtureTests!backend(
+            buildPath(cerealTestsDir, "cerealiser_impl.d"),
+            dubImportPaths,
+        );
+    }
+
     @("cerealed.compile_time.d." ~ backend.stringof)
     unittest {
         runBackendFileFixtureTests!backend(
