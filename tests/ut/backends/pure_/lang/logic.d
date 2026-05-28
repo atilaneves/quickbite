39736d838c300d21c72c779f00724242bdfb57d2
Atila Neves <atila.neves@gmail.com>
2026-05-28 11:21:50 +0200

    Migrate CTFE nonzero int condition test

---
+++ tests/ut/backends/pure_/lang/logic.d
@@ -0,0 +1,48 @@
+module ut.backends.pure_.lang.logic;
+
+
+import ut.backends;
+
+
+private:
+
+static foreach (backend; backends) {
+    @("assertNonzeroIntCondition." ~ backend.stringof)
+    unittest {
+        newBackend!backend.runTests(q{
+            int mask() {
+                return 2;
+            }
+
+            unittest {
+                assert(0x28 | mask);
+            }
+        });
+    }
+
+    @("assertNonzeroIntConditionFailureMessage.0." ~ backend.stringof)
+    unittest {
+        newBackend!backend.runTests(q{
+            int mask() {
+                return 2;
+            }
+
+            unittest {
+                assert((0x28 | mask) == 0x2b);
+            }
+        }).shouldThrowWithMessage("42 != 43");
+    }
+
+    @("assertNonzeroIntConditionFailureMessage.1." ~ backend.stringof)
+    unittest {
+        newBackend!backend.runTests(q{
+            int mask() {
+                return 1;
+            }
+
+            unittest {
+                assert((0x28 | mask) == 0x2a);
+            }
+        }).shouldThrowWithMessage("41 != 42");
+    }
+}
