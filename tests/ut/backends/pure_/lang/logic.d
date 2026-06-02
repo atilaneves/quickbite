f203568143fb4018ab9879a70f41bc96b93fa3f9
Atila Neves <atila.neves@gmail.com>
2026-06-02 17:59:16 +0200

    Run logicalAndFailureMessage 1 with interpreter

--- tests/ut/backends/pure_/lang/logic.d
+++ tests/ut/backends/pure_/lang/logic.d
@@ -72,17 +72,6 @@ static foreach (backend; backends) {
         }).shouldThrowWithMessage("false != true");
     }
 
-    @("logicalAndFailureMessage.1." ~ backend.stringof)
-    unittest {
-        runBackendSourceFixtureTests!backend(q{
-            unittest {
-                bool left = true;
-                bool right = false;
-                assert((left && right) == true);
-            }
-        }).shouldThrowWithMessage("false != true");
-    }
-
     @("logicalAndCall." ~ backend.stringof)
     unittest {
         runBackendSourceFixtureTests!backend(q{
@@ -371,6 +360,17 @@ static foreach (backend; backends) {
 }
 
 static foreach (backend; backendsWith!Interpreter) {
+    @("logicalAndFailureMessage.1." ~ backend.stringof)
+    unittest {
+        runBackendSourceFixtureTests!backend(q{
+            unittest {
+                bool left = true;
+                bool right = false;
+                assert((left && right) == true);
+            }
+        }).shouldThrowWithMessage("false != true");
+    }
+
     @("logicalAndFailureMessage.0." ~ backend.stringof)
     unittest {
         runBackendSourceFixtureTests!backend(q{
