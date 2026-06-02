992adca9c3691d81c762f7e4cef90236f1611293
Atila Neves <atila.neves@gmail.com>
2026-06-02 07:36:06 +0200

    Promote tree walker add int eval

--- tests/ut/backends/pure_/lang/eval.d
+++ tests/ut/backends/pure_/lang/eval.d
@@ -18,14 +18,14 @@ static foreach (backend; backendsWith!(Bytecode, TreeWalker)) {
     unittest {
         newBackend!backend.eval("1 + 2").should == Value(3);
     }
-}
 
-static foreach (backend; backendsWith!Bytecode) {
     @("add.int.1." ~ backend.stringof)
     unittest {
         newBackend!backend.eval("2 + 2").should == Value(4);
     }
+}
 
+static foreach (backend; backendsWith!Bytecode) {
     @("add.int.2." ~ backend.stringof)
     unittest {
         newBackend!backend.eval("3 + 3").should == Value(6);
