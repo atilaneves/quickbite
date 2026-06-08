348106e563334810b547390be474acd2310dcdf6
Atila Neves <atila.neves@gmail.com>
2026-06-08 18:45:34 +0200

    Simplify bytecode integrals coverage loop

--- tests/ut/backends/lang/integrals.d
+++ tests/ut/backends/lang/integrals.d
@@ -20,9 +20,6 @@ private alias IntegralTypes = AliasSeq!(
 
 static foreach (backend; AliasSeq!(Bytecode)) {
     static foreach (T; IntegralTypes) {
-        static if (is(T == byte) || is(T == ubyte) || is(T == short) ||
-            is(T == ushort) || is(T == int) || is(T == uint) ||
-            is(T == long) || is(T == ulong))
         @("type." ~ T.stringof ~ "." ~ Bytecode.stringof)
         unittest {
             runBackendSourceFixtureTests!Bytecode(text(
