081daaeeed9cfe4acbe607a74bfbebf40e9e93e4
Atila Neves <atila.neves@gmail.com>
2026-06-12 19:25:41 +0200

    Promote function parameter diagnostic to bytecode new core

--- tests/ut/backends/runner/ct/diagnostics.d
+++ tests/ut/backends/runner/ct/diagnostics.d
@@ -241,7 +241,7 @@ static foreach (backend; AliasSeq!(Ctfe, Interpreter, Bytecode, SystemLinker)) {
     }
 }
 
-static foreach (backend; AliasSeq!(Ctfe, Interpreter, Bytecode, SystemLinker)) {
+static foreach (backend; AliasSeq!(Ctfe, Interpreter, Bytecode, BytecodeNewCore, SystemLinker)) {
     @("functionParameterOops." ~ backend.stringof)
     @Tags(backend.stringof)
     unittest {
