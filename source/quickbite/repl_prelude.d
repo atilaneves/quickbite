cb62118f991ebfde3c3e4d83c8870ae149be4573
Atila Neves <atila.neves@gmail.com>
2026-06-19 04:41:55 -0400

    Extend REPL formatter numeric suffixes

--- source/quickbite/repl_prelude.d
+++ source/quickbite/repl_prelude.d
@@ -3,20 +3,39 @@ module quickbite.repl_prelude;
 private:
 
 public string __quickbiteFormat(T)(in T value) @safe pure {
-    import std.algorithm.searching: canFind;
     import std.conv: text;
+    import std.traits: Unqual;
 
-    static if (is(T == char)) {
+    alias U = Unqual!T;
+
+    static if (is(U == char)) {
         return text("'", value, "'");
-    } else static if (is(T == string)) {
+    } else static if (is(U == string)) {
         return text(`"`, value, `"`);
-    } else static if (is(T == double)) {
-        const rendered = text(value);
-        if (rendered.canFind(".") || rendered.canFind("e") ||
-            rendered.canFind("E"))
-            return rendered;
-        return text(rendered, ".0");
+    } else static if (is(U == uint)) {
+        return text(value, "u");
+    } else static if (is(U == long)) {
+        return text(value, "L");
+    } else static if (is(U == ulong)) {
+        return text(value, "UL");
+    } else static if (is(U == float)) {
+        return text(floatingDisplay(value), "f");
+    } else static if (is(U == double)) {
+        return floatingDisplay(value);
+    } else static if (is(U == real)) {
+        return text(floatingDisplay(value), "L");
     } else {
         return text(value);
     }
+}
+
+private string floatingDisplay(T)(in T value) @safe pure {
+    import std.algorithm.searching: canFind;
+    import std.conv: text;
+
+    const rendered = text(value);
+    if (rendered.canFind(".") || rendered.canFind("e") ||
+        rendered.canFind("E"))
+        return rendered;
+    return text(rendered, ".0");
 }
