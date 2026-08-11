6c2ba75693520f579f8e3fd137892c755caacbe7
Atila Neves <atila.neves@gmail.com>
2026-08-11 12:01:27 +0200

    Run fast CI checks first

--- ci.sh
+++ ci.sh
@@ -5,9 +5,9 @@ cd "$(git -C "$(dirname -- "${BASH_SOURCE[0]}")" rev-parse --show-toplevel)"
 if [[ ! -f build.ninja ]]; then
     dub run reggae --compiler=ldc -- -b ninja
 fi
-ninja bin/ut
-bin/ut --random ~@LLVMJit ~@Ctfe
 dmd -unittest -checkaction=context -main -run tests/example.d
 bin/bench.sh -b interpreter -b system-linker --dub cerealed -w 0 -r 1
 ninja bin/qb
 uv run tests/run_repl.py
+ninja bin/ut
+bin/ut --random ~@LLVMJit ~@Ctfe
