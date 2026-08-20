8a988ac0b0955368ddc0fc1921f81452179620c4
Atila Neves <atila.neves@gmail.com>
2026-08-20 18:52:16 +0200

    Add bytecode to cerealed benchmarks in ci.sh

--- ci.sh
+++ ci.sh
@@ -6,8 +6,8 @@ if [[ ! -f build.ninja ]]; then
     dub run reggae --compiler=ldc -- -b ninja
 fi
 dmd -unittest -checkaction=context -main -run tests/example.d
-bin/bench.sh
-bin/bench.sh -b interpreter -b system-linker --dub cerealed -w 0 -r 1
+bin/bench.sh  # example.d
+bin/bench.sh -b system-linker -b interpreter -b bytecode -w 0 -r 1 --dub cerealed
 ninja bin/qb
 uv run tests/run_repl.py
 ninja bin/ut
